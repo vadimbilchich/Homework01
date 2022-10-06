0f4335596a35fe3afa85c66cc9dc0430d582ce38
VadimBilchich <vadimbilchich@rambler.ru>
2022-10-06 11:41:02 +0800

    Добавили решение задачи 4

---
+++ task04/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int a = 22;
+int b = 3;
+int c = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
+
